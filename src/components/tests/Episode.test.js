82318533aab767cd148ea55493af7327a3f0d532
Karla Nieves <karlaxnieves@gmail.com>
2021-06-14 14:46:40 -0700

    working on the episode testing

--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -13,19 +13,30 @@ const testEpisode = {
 }
 
 const testEpisodeWithoutImage = {
-    //Add in approprate test data structure here.
+    id:1,
+    name: "",
+    image: null,
+    season: 1,
+    number: 1,
+    summary: "test",
+    runtime: 1
 }
 
 test("renders without error", () => {
-
+render(<Episode episode={testEpisode}/>);
 });
 
-test("renders the summury test passed as prop", ()=>{
-    
+test("renders the summary test passed as prop", ()=>{
+    // render(<Episode episode={testEpisode}/>);
+    // const summary = screen.queryByText(/test/i)
+    // expect(summary).toBeTruthy();
+    // expect(summary).toHaveTextContent(/test/i)
 });
 
 test("renders default image when image is not defined", ()=>{
-    
+    render(<Episode episode={testEpisodeWithoutImage} />)
+    const image = screen.queryByAltText('./stranger_things.png');
+    expect(image).toBeInTheDocument();
 })
 
 //Tasks
